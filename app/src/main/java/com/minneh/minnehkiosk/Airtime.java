3b5c032724f06364f08a8d2016652b0a491c9911
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-19 16:50:50 +0300

    add airtime activity and layout skeleton

---
+++ app/src/main/java/com/minneh/minnehkiosk/Airtime.java
@@ -0,0 +1,13 @@
+package com.minneh.minnehkiosk;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Airtime extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_airtime);
+    }
+}
