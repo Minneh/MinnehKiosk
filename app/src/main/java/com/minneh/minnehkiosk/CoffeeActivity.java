5416f1becc7df2e7e8fa884d569197e9748d7dd5
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-22 16:44:44 +0300

    update layout with number picker for amount and bind views in CoffeeActivity

--- app/src/main/java/com/minneh/minnehkiosk/CoffeeActivity.java
+++ app/src/main/java/com/minneh/minnehkiosk/CoffeeActivity.java
@@ -1,13 +1,29 @@
 package com.minneh.minnehkiosk;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
-public class CoffeeActivity extends AppCompatActivity {
+import butterknife.Bind;
+
+public class CoffeeActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = CoffeeActivity.class.getSimpleName();
+
+    @Bind(R.id.whiteNumberPicker) NumberPicker mWhiteNumberPicker;
+    @Bind(R.id.whiteCoffeeButton) Button mWhiteButton;
+    @Bind(R.id.blackNumberPicker) NumberPicker mBlackNumberPicker;
+    @Bind(R.id.blackCoffeeButton) EditText mBlackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee);
+
+
     }
+
 }
